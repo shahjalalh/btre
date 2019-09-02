5717b603481d503f37b6de5cb48ed402bd574c33
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2019-09-02 22:17:40 +0600

    accounts

--- btre/settings.py
+++ btre/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # system 
+    # system
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
